9bc38ad7784eebbc3189bb210a8b099f78caa408
casey060492 <casey060492@gmail.com>
2016-10-05 03:20:18 +0000

    second commit

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -1,2 +1,20 @@
-var aloha = 'Aloha Variables!';
-console.log(aloha);
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+    res.send('Aloha World!');
+});
+
+app.get('/routing', function(req, res){
+    res.send('Aloha Routing!');
+});
+
+var port = process.env.PORT;
+app.listen(port, function(err){
+    console.log('The server is running on port: ' + port);
+    
+});
+
+app.use(express.static('public'));
+
+app.use(express.static('src/views'));
